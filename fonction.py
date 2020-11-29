0b242f967577fb1726f0175f01c24322cc3fb545
Cédric <cedricyoganathan@gmail.com>
2020-11-29 13:10:00 +0100

    intial

---
+++ fonction.py
@@ -0,0 +1,29 @@
+from time import sleep
+import numpy as np
+import simpleaudio as sa
+
+def calc_frequency(notes, frequences):
+    #TODO
+    return
+
+def calc_duration(figures, d0):
+
+    #TODO
+
+    return
+
+def read_line_file(f, num):
+
+    #TODO
+
+    return
+
+def read_sheet():
+    #TODO
+
+    return
+
+def play_sheet():
+    #TODO
+
+    return
